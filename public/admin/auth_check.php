837ac44ec0ded0c58c24edfdcdaa98df1da0060a
arutemyan <46350701+arutemyan@users.noreply.github.com>
2025-11-08 16:18:00 +0900

    fix test

--- public/admin/auth_check.php
+++ public/admin/auth_check.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  */
 
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/_feature_check.php';
 $config = \App\Config\ConfigManager::getInstance()->getConfig();
 require_once __DIR__ . '/../../src/Security/SecurityUtil.php';
 
